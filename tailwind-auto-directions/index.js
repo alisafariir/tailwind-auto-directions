424d180e01b823b44933477eea0a5e8f50a6fce1
asafari72 <asafari72@gmail.com>
2023-04-03 17:43:52 +0330

    const

--- tailwind-auto-directions/index.js
+++ tailwind-auto-directions/index.js
@@ -1,5 +1,6 @@
 const plugin = require("tailwindcss/plugin");
-const TailwindRTLUtilites = plugin(function ({ matchUtilities, theme }) {
+
+const spacing = plugin(function ({ matchUtilities, theme }) {
   matchUtilities(
     {
       ps: (value) => ({
@@ -21,77 +22,86 @@ const TailwindRTLUtilites = plugin(function ({ matchUtilities, theme }) {
         }),
       },
       { values: theme("spacing") }
-    ),
-    matchUtilities(
-      {
-        "dir-rtl": () => ({
-          direction: "rtl",
-        }),
-        "dir-ltr": () => ({
-          direction: "ltr",
-        }),
-      },
-      {}
-    ),
-    matchUtilities(
-      {
-        "text-start": () => ({
-          "text-align": "start",
-        }),
-        "text-end": () => ({
-          "text-align": "end",
-        }),
-      },
-      {}
-    ),
-    matchUtilities(
-      {
-        "border-s": (value) => ({
-          borderInlineStartWidth: value,
-        }),
-        "border-e": (value) => ({
-          borderInlineEndWidth: value,
-        }),
-      },
-      { values: theme("borderWidth") }
-    ),
-    matchUtilities(
-      {
-        "rounded-s": (value) => ({
-          borderStartStartRadius: value,
-          borderEndStartRadius: value,
-        }),
-        "rounded-e": (value) => ({
-          borderStartEndRadius: value,
-          borderEndEndRadius: value,
-        }),
-        "rounded-ts": (value) => ({
-          borderStartStartRadius: value,
-        }),
-        "rounded-te": (value) => ({
-          borderStartEndRadius: value,
-        }),
-        "rounded-bs": (value) => ({
-          borderEndStartRadius: value,
-        }),
-        "rounded-be": (value) => ({
-          borderEndEndRadius: value,
-        }),
-      },
-      { values: theme("borderRadius") }
-    ),
-    matchUtilities(
-      {
-        start: (value) => ({
-          insetInlineStart: value,
-        }),
-        end: (value) => ({
-          insetInlineEnd: value,
-        }),
-      },
-      { values: theme("inset") }
-    ),
-   
+    );
+});
+const directions = plugin(function ({ matchUtilities }) {
+  matchUtilities(
+    {
+      "dir-rtl": () => ({
+        direction: "rtl",
+      }),
+      "dir-ltr": () => ({
+        direction: "ltr",
+      }),
+    },
+    {}
+  );
+});
+const textAligns = plugin(function ({ matchUtilities }) {
+  matchUtilities(
+    {
+      "text-start": () => ({
+        "text-align": "start",
+      }),
+      "text-end": () => ({
+        "text-align": "end",
+      }),
+    },
+    {}
+  );
+});
+const borders = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      "border-s": (value) => ({
+        borderInlineStartWidth: value,
+      }),
+      "border-e": (value) => ({
+        borderInlineEndWidth: value,
+      }),
+    },
+    { values: theme("borderWidth") }
+  );
+});
+const radius = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      "rounded-s": (value) => ({
+        borderStartStartRadius: value,
+        borderEndStartRadius: value,
+      }),
+      "rounded-e": (value) => ({
+        borderStartEndRadius: value,
+        borderEndEndRadius: value,
+      }),
+      "rounded-ts": (value) => ({
+        borderStartStartRadius: value,
+      }),
+      "rounded-te": (value) => ({
+        borderStartEndRadius: value,
+      }),
+      "rounded-bs": (value) => ({
+        borderEndStartRadius: value,
+      }),
+      "rounded-be": (value) => ({
+        borderEndEndRadius: value,
+      }),
+    },
+    { values: theme("borderRadius") }
+  );
+});
+const alignments = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      start: (value) => ({
+        insetInlineStart: value,
+      }),
+      end: (value) => ({
+        insetInlineEnd: value,
+      }),
+    },
+    { values: theme("inset") }
+  ),
     matchUtilities(
       {
         "float-start": () => ({
@@ -115,4 +125,29 @@ const TailwindRTLUtilites = plugin(function ({ matchUtilities, theme }) {
       {}
     );
 });
+const divides = plugin(function ({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      x: (value) => ({
+        borderInlineStartWidth: value,
+        borderInlineEndWidth: value,
+      }),
+      y: (value) => ({
+        borderBlockStartWidth: value,
+        borderBlockEndWidth: value,
+      }),
+    },
+    { values: theme("divide") }
+  );
+});
+
+const TailwindRTLUtilites = {
+  spacing,
+  directions,
+  textAligns,
+  borders,
+  radius,
+  alignments,
+  divides,
+};
 module.exports = TailwindRTLUtilites;
